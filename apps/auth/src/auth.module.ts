b0ed2e596fa68854d979acbe3c75d4c1df0837c9
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2025-04-20 10:45:43 +0300

    feat(auth): validate env variables

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -4,12 +4,20 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users';
 import { PrismaModule, PrismaService } from '@app/common';
 import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
   imports: [
     PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        DATABASE_URL: Joi.string().required(),
+      }),
     }),
   ],
   controllers: [AuthController],
