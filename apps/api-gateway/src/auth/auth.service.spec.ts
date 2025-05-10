8f918934981493a80874b04d2bd62d8691a5ac7d
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2025-05-10 17:21:38 +0300

    test(auth): test addRefreshTokenToResponse function

---
+++ apps/api-gateway/src/auth/auth.service.spec.ts
@@ -0,0 +1,63 @@
+import { AuthService } from './auth.service';
+import { Test } from '@nestjs/testing';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Response } from 'express';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let resMock: Partial<Response>;
+  let expectedRefreshToken: string;
+  let configServiceMock: DeepMockProxy<ConfigService>;
+  let mockDays: string;
+
+  beforeEach(async () => {
+    mockDays = '7';
+    configServiceMock = mockDeep<ConfigService>();
+    configServiceMock.get.mockReturnValue(mockDays);
+    resMock = {
+      cookie: jest.fn(),
+      status: jest.fn().mockReturnValue(200),
+      send: jest.fn(),
+    };
+    expectedRefreshToken = 'refresh-token';
+
+    const module = await Test.createTestingModule({
+      imports: [await ConfigModule.forRoot({ isGlobal: true })],
+      providers: [
+        AuthService,
+        {
+          provide: ConfigService,
+          useValue: configServiceMock,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  describe('addRefreshTokenToResponse', () => {
+    it('should call res.cookie with correct params', () => {
+      const expectedExpiresIn = new Date();
+      expectedExpiresIn.setDate(
+        expectedExpiresIn.getDate() + parseInt(mockDays),
+      );
+
+      service.addRefreshTokenToResponse(
+        resMock as Response,
+        expectedRefreshToken,
+      );
+
+      expect(resMock.cookie).toHaveBeenCalledWith(
+        'refreshToken',
+        expectedRefreshToken,
+        {
+          expires: expectedExpiresIn,
+          httpOnly: true,
+          sameSite: 'lax',
+          secure: true,
+        },
+      );
+    });
+  });
+});
