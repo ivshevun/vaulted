1bd4f55528749f374b29a24b7fd5271fedcd8a20
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2025-06-01 15:53:39 +0300

    chore(api-gateway): delete antivirus from api-gateway

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -4,7 +4,6 @@ import { FilesController } from './files/files.controller';
 import { FilesModule } from './files/files.module';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
-import { AntivirusController } from './antivirus/antivirus.controller';
 
 @Module({
   imports: [
@@ -37,25 +36,11 @@ import { AntivirusController } from './antivirus/antivirus.controller';
         }),
         inject: [ConfigService],
       },
-      {
-        name: 'antivirus',
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [configService.get<string>('RABBITMQ_URL')!],
-            queue: 'antivirus_queue',
-            queueOptions: {
-              durable: true,
-            },
-          },
-        }),
-        inject: [ConfigService],
-      },
     ]),
     AuthModule,
     FilesModule,
   ],
-  controllers: [FilesController, AntivirusController],
+  controllers: [FilesController],
   providers: [],
 })
 export class ApiGatewayModule {}
