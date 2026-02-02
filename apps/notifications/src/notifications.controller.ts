e00e60e5c95e6035827889ead61a722d7b371649
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2026-02-02 08:44:51 +0200

    chore: fix lint

--- apps/notifications/src/notifications.controller.ts
+++ apps/notifications/src/notifications.controller.ts
@@ -1,13 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { SendMailDto } from '@app/common';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
   @Post()
-  sendMail(@Body() dto: SendMailDto) {
+  sendMail() {
     return this.notificationsService.sendMail();
   }
 }
