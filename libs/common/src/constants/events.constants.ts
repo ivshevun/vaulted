43c6081dd6dac216c392fe59ca4c93018ca69389
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2026-04-14 21:27:37 +0300

    feat(files): add file size limit and scan skipped constants

--- libs/common/src/constants/events.constants.ts
+++ libs/common/src/constants/events.constants.ts
@@ -3,3 +3,4 @@ export const FILE_SCAN_STARTED = 'file.scan.started';
 export const FILE_SCAN_INFECTED = 'file.scan.infected';
 export const FILE_SCAN_CLEAR = 'file.scan.clear';
 export const FILE_SCAN_FAILED = 'file.scan.failed';
+export const FILE_SCAN_SKIPPED = 'file.scan.skipped';
