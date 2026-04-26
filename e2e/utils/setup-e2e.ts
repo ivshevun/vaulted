a022db700e74eef01d753e7273ddd5db75a4543f
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2026-04-26 17:20:09 +0300

    refactor(e2e): rewrite setup-e2e to connect via external URL

--- e2e/utils/setup-e2e.ts
+++ e2e/utils/setup-e2e.ts
@@ -1,34 +1,3 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ApiGatewayModule } from '@apps/api-gateway/src/api-gateway.module';
-import { HttpRpcExceptionInterceptor } from '@apps/api-gateway/src/auth/src/interceptors';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
-import { Server } from 'http';
-import {
-  PrismaModule as FilesPrismaModule,
-  PrismaService as FilesPrismaService,
-} from '@apps/files/src/prisma';
-import {
-  PrismaModule as AuthPrismaModule,
-  PrismaService as AuthPrismaService,
-} from '@apps/auth/src/prisma';
-
-export async function setupE2e() {
-  const moduleFixture: TestingModule = await Test.createTestingModule({
-    imports: [ApiGatewayModule, FilesPrismaModule, AuthPrismaModule],
-    providers: [FilesPrismaService, AuthPrismaService],
-  }).compile();
-
-  const app = moduleFixture.createNestApplication();
-  app.setGlobalPrefix('api', { exclude: ['health'] });
-  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
-  app.useGlobalInterceptors(new HttpRpcExceptionInterceptor());
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.use(cookieParser());
-
-  const httpServer = app.getHttpServer() as Server;
-
-  await app.init();
-
-  return { app, httpServer };
+export function setupE2e() {
+  return { baseUrl: process.env.API_GATEWAY_URL! };
 }
